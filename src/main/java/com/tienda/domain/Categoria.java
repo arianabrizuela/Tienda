6ccef634f025f17db3d3374bfe8cd6dc972847ae
arianabrizuela <abrizuela2004@gmail.com>
2024-03-07 14:53:26 -0600

    Semana 5 commit del proyecto Tienda

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,32 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity 
+@Table(name="Categoria")
+
+public class Categoria implements Serializable{  
+    
+    private static final long serialVersionUID = 1L; //incrementar uno a uno el valor 
+    
+    @Id //llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria") //el nombre correcto del idCategoria en la base de datos
+    private long idCategoria;
+    
+    private String descripcion;
+    private String ruta_imagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+}
---
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,25 @@
+package com.tienda.service.impl;
+
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.tienda.service.CategoriaService;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+    
+    //La anotacion autowired crea un unico objeto mientras se ejecuta la app
+    @Autowired
+    private CategoriaDao categoriaDao;
+    
+    public List<Categoria> getCategorias(boolean activos){
+        var lista=categoriaDao.findAll();
+        if(activos){ //se deben eliminar los que no estan activos
+            lista.removeIf(e -> !e.isActivo());
+                       //de la lista elimine los que no estan activos
+        }
+        return lista;
+    }
+}
